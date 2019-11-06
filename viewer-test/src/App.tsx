e2819d81be7d5375ac4457e0662c6e7e0f8b14a9
pondelion <programming.deve@gmail.com>
2019-11-07 05:00:20 +0900

    add burger menu

---
+++ viewer-test/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BubbleMenu from './Menu/BubbleMenu';
+import './App.css';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <BubbleMenu></BubbleMenu>
+    </div>
+  );
+}
+
+export default App;
